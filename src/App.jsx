26997bb5e01c43294ee5d74dc7999b42979be0a7
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-08-04 23:40:01 +0200

    PersonalData

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css' //importing Styling
 import Copyright from './components/Questions/Copyright/Copyright.jsx';
+import PersonalData from './components/Questions/PersonalData/PersonalData.jsx';
 
 function App() {
   return (
     <>
       <div className='LeftSide'>
         <Copyright />
-
+        <PersonalData />
       </div>
     </>
   )
---
+++ src/Components/Questions/PersonalData/PersonalData.jsx
@@ -0,0 +1,20 @@
+import './PersonalData.css'
+
+function PersonalData(){
+    return (
+        <>
+            <div className='App'>
+                <p className='text'>Personal Data</p>
+                <div className="Initials">
+                    <input placeholder="FirstName"/>
+                    <input placeholder="LastName"/>
+                </div>
+                <input className='Niche' placeholder='Niche: Software Developer' />
+                <br />
+                <input  className='Info' placeholder='Something about yourself... ' />
+            </div>
+        </>
+    )
+}
+
+export default PersonalData;
