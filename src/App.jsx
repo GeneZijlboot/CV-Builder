11c4ec7d3519b62237a38560f335ee83fd0e483d
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-08-04 22:33:37 +0200

    Setting up all the Components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css' //importing Styling
+import ListGroup from './components/ListGroup';
+
+function App() {
+  return (
+    <>
+      <div><ListGroup /></div>
+    </>
+  )
+}
+
+export default App;
